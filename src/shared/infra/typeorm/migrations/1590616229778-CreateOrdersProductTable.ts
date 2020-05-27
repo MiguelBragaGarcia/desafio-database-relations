71947e8f703b3176e7104d535dfbfe701a635acd
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-05-27 19:11:54 -0300

    Criando as tabelas do banco de dados

---
+++ src/shared/infra/typeorm/migrations/1590616229778-CreateOrdersProductTable.ts
@@ -0,0 +1,76 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersProductTable1590616229778
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+
+          {
+            name: 'product_id',
+            type: 'uuid',
+          },
+
+          {
+            name: 'order_id',
+            type: 'uuid',
+          },
+
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'productReference',
+            columnNames: ['product_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'products',
+          },
+
+          {
+            name: 'orderReference',
+            columnNames: ['order_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'orders',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
