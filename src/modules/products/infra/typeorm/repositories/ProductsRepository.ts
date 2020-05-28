60b061c1e6eb0e8cfaa4de311fcc0e70beef687d
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-05-28 19:51:49 -0300

    Implementando o módulo de produtos

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,50 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const createProduct = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    const savedProduct = await this.ormRepository.save(createProduct);
+
+    return savedProduct;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const hasProduct = await this.ormRepository.findOne({ where: { name } });
+
+    return hasProduct;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const allProductsInArrayId = await this.ormRepository.findByIds(products);
+
+    return allProductsInArrayId;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const ids = products.map(product => product.id);
+
+    const storedProducts = await this.ormRepository.findByIds(ids);
+
+    const updatedProducts = storedProducts.map(storedProduct => {
+      const productQuantities = products.find(
+        product => product.id === storedProduct.id,
+      );
+
+      return {
+        ...storedProduct,
+        quantity: productQuantities?.quantity,
+      };
+    });
+
+    const savedProducts = await this.ormRepository.save(updatedProducts);
+
+    return savedProducts;
   }
 }
 
