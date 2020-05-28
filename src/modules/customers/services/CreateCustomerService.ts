0582822337509a706e323839804b3ec86a94a8da
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-05-28 19:51:18 -0300

    Implementando o módulo de customers

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const hasCustomer = await this.customersRepository.findByEmail(email);
+
+    if (hasCustomer) {
+      throw new AppError('Customer already registered');
+    }
+
+    const newCustomer = await this.customersRepository.create({ name, email });
+
+    return newCustomer;
   }
 }
 
