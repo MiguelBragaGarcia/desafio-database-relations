2ee3eb1f6af03571b1ca993431b89cb895802a7a
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-05-29 17:43:32 -0300

    Criando as funcionalidades e regras de negócio da aplicação

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,73 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const hasCustomer = await this.customersRepository.findById(customer_id);
+
+    if (!hasCustomer) {
+      throw new AppError('This customer does not exists.');
+    }
+
+    const hasProducts = await this.productsRepository.findAllById(products);
+
+    if (hasProducts.length !== products.length) {
+      throw new AppError('There are invalid products on this list.');
+    }
+
+    const negativeQuantitie = products
+      .map(product => product.quantity)
+      .some(quantity => quantity < 0);
+
+    if (negativeQuantitie) {
+      throw new AppError('Negative product quantities are invalid');
+    }
+
+    const updatedListQuantity = hasProducts.map(product => {
+      const productToBeUpdated = products.find(prod => prod.id === product.id);
+
+      if (!productToBeUpdated) {
+        return {
+          id: product.id,
+          quantity: -1,
+        };
+      }
+
+      return {
+        id: product.id,
+        quantity: product.quantity - productToBeUpdated?.quantity,
+      };
+    });
+
+    if (updatedListQuantity.some(product => product.quantity < 0)) {
+      throw new AppError('This product does not have this quantity in stock.');
+    }
+
+    const formattedProductList = products.map((product, index) => {
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        price: hasProducts[index].price,
+      };
+    });
+
+    const createOrder = await this.ordersRepository.create({
+      customer: hasCustomer,
+      products: formattedProductList,
+    });
+
+    await this.productsRepository.updateQuantity(updatedListQuantity);
+
+    return createOrder;
   }
 }
 
